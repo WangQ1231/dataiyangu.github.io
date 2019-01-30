a9b954dee3e45d86774d2a2c2255307e550527c7
dataiyangu <32248827@qq.com>
2019-01-30 09:42:44 +0800

    Site updated: 2019-01-30 09:42:43

---
+++ js/src/pjax/lazyload.js
@@ -0,0 +1,3 @@
+// build time:Wed Jan 30 2019 09:42:40 GMT+0800 (China Standard Time)
+function lazy(){$("img").lazyload({threshold:0,placeholder:"images/my_blog_0.png",effect:"fadeIn"})}lazy();
+//rebuild by neat 
---
+++ static/api/js/share/likeshare.js
@@ -0,0 +1,3 @@
+// build time:Wed Jan 30 2019 09:42:40 GMT+0800 (China Standard Time)
+
+//rebuild by neat 
