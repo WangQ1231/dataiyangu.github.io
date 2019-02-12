f1580878e4250fa5d467d88e95484b2d61e3449d
dataiyangu <32248827@qq.com>
2019-02-12 15:10:01 +0800

    Site updated: 2019-02-12 15:10:00

--- js/src/category.js
+++ js/src/category.js
@@ -1,3 +1,3 @@
-// build time:Tue Feb 12 2019 15:06:24 GMT+0800 (China Standard Time)
+// build time:Tue Feb 12 2019 15:09:57 GMT+0800 (China Standard Time)
 $(document).ready(function(){i();function i(){$(".category-list-link").filter(function(){return $(this).siblings(".category-list-child").length>0}).attr("href","javascript:void(0)");$(".category-list-link").click(function(){$(this).siblings(".category-list-child").slideToggle()});var i=$(window).width()/1.2;$(".category-list-link").animate({width:i},300)}});
 //rebuild by neat 
--- static/api/js/trans/data.js
+++ static/api/js/trans/data.js
@@ -1,3 +1,3 @@
-// build time:Tue Feb 12 2019 15:06:25 GMT+0800 (China Standard Time)
+// build time:Tue Feb 12 2019 15:09:58 GMT+0800 (China Standard Time)
 
 //rebuild by neat 
