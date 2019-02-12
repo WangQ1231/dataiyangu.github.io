144386ddf00c57222012f0bb97fbd2de89ebaa17
dataiyangu <32248827@qq.com>
2019-02-12 15:45:40 +0800

    Site updated: 2019-02-12 15:45:39

--- js/src/pjax/lazyload.js
+++ js/src/pjax/lazyload.js
@@ -1,3 +1,3 @@
-// build time:Tue Feb 12 2019 15:41:29 GMT+0800 (China Standard Time)
+// build time:Tue Feb 12 2019 15:45:37 GMT+0800 (China Standard Time)
 function lazy(){$("img").lazyload({threshold:0,placeholder:"images/my_blog_0.png",effect:"fadeIn"})}lazy();
 //rebuild by neat 
