15f60ce6b763d26dc662370dd81ffd67f5190f4c
dataiyangu <32248827@qq.com>
2019-03-28 14:42:24 +0800

    Site updated: 2019-03-28 14:42:22

--- js/src/pjax/scrollReveal/my_scrollReveal.js
+++ js/src/pjax/scrollReveal/my_scrollReveal.js
@@ -1,3 +1,3 @@
-// build time:Thu Mar 28 2019 14:30:59 GMT+0800 (China Standard Time)
+// build time:Thu Mar 28 2019 14:42:18 GMT+0800 (China Standard Time)
 var pathname=window.location.pathname;if(pathname.indexOf("html")<0){var config={enter:"right",move:"40px",over:"3s",after:"5",reset:true,init:true};window.scrollReveal=new scrollReveal(config);scrollReveal.init()}
 //rebuild by neat 
--- js/src/pjax/share.js
+++ js/src/pjax/share.js
@@ -1,3 +1,3 @@
-// build time:Thu Mar 28 2019 14:30:59 GMT+0800 (China Standard Time)
+// build time:Thu Mar 28 2019 14:42:18 GMT+0800 (China Standard Time)
 define(["jquery.share.min"],function(){var e={sites:["weibo","qq","wechat","qzone","douban","facebook","google","twitter"],disabled:["linkedin","diandian"],wechatQrcodeTitle:"微信扫一扫",wechatQrcodeHelper:"<p>微信扫一扫，右上角分享</p>",source:"Leesin Dong"};$(".post-spread").share(e);function o(){$(".post-spread").share(e)}});
 //rebuild by neat 
