d560d2cffa600a375b47c788d8ff13723711478c
dataiyangu <32248827@qq.com>
2019-03-28 14:24:23 +0800

    Site updated: 2019-03-28 14:24:21

--- live/autoload.js
+++ live/autoload.js
@@ -1,3 +1,3 @@
-// build time:Wed Mar 27 2019 23:02:49 GMT+0800 (China Standard Time)
+// build time:Thu Mar 28 2019 14:24:16 GMT+0800 (China Standard Time)
 $("<link>").attr({href:"/live/waifu.css",rel:"stylesheet",type:"text/css"}).appendTo("head");$.ajax({url:"/live/live2d.min.js",dataType:"script",cache:true,async:false});$.ajax({url:"/live/waifu-tips.js",dataType:"script",cache:true,async:false});$(window).on("load",function(){initWidget("/live/","https://www.mmmmmm.me/live2d_api")});
 //rebuild by neat 
