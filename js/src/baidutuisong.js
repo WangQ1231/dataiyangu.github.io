e3824d67d3ff849bcccb6ffc9177b8981ece5a99
dataiyangu <32248827@qq.com>
2019-03-28 22:06:33 +0800

    Site updated: 2019-03-28 22:06:31

--- js/src/baidutuisong.js
+++ js/src/baidutuisong.js
@@ -1,3 +1,3 @@
-// build time:Thu Mar 28 2019 14:42:18 GMT+0800 (China Standard Time)
+// build time:Thu Mar 28 2019 22:06:28 GMT+0800 (China Standard Time)
 (function(){var t=document.createElement("script");var e=window.location.protocol.split(":")[0];if(e==="https"){t.src="https://zz.bdstatic.com/linksubmit/push.js"}else{t.src="http://push.zhanzhang.baidu.com/push.js"}var s=document.getElementsByTagName("script")[0];s.parentNode.insertBefore(t,s)})();
 //rebuild by neat 
--- live/autoload.js
+++ live/autoload.js
@@ -1,3 +1,3 @@
-// build time:Thu Mar 28 2019 14:42:18 GMT+0800 (China Standard Time)
+// build time:Thu Mar 28 2019 22:06:28 GMT+0800 (China Standard Time)
 $("<link>").attr({href:"/live/waifu.css",rel:"stylesheet",type:"text/css"}).appendTo("head");$.ajax({url:"/live/live2d.min.js",dataType:"script",cache:true,async:false});$.ajax({url:"/live/waifu-tips.js",dataType:"script",cache:true,async:false});$(window).on("load",function(){initWidget("/live/","https://www.mmmmmm.me/live2d_api")});
 //rebuild by neat 
