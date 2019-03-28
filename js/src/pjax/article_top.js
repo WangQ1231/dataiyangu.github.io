e231344e6d39becaf4e202bef46da56a9ab2ca4a
dataiyangu <32248827@qq.com>
2019-03-28 23:38:22 +0800

    Site updated: 2019-03-28 23:38:20

--- js/src/pjax/article_top.js
+++ js/src/pjax/article_top.js
@@ -1,3 +1,3 @@
-// build time:Thu Mar 28 2019 22:06:28 GMT+0800 (China Standard Time)
+// build time:Thu Mar 28 2019 23:38:18 GMT+0800 (China Standard Time)
 function article_top(){function t(){var t=window.location.pathname;var i=$(".header").css("height");if(t.indexOf("html")>0){$("html,article").animate({scrollTop:i},300)}}setTimeout(t,800)}article_top();
 //rebuild by neat 
--- js/src/pjax/lazyload.js
+++ js/src/pjax/lazyload.js
@@ -1,3 +1,3 @@
-// build time:Thu Mar 28 2019 22:06:28 GMT+0800 (China Standard Time)
+// build time:Thu Mar 28 2019 23:38:18 GMT+0800 (China Standard Time)
 function lazy(){$("img").lazyload({threshold:0,placeholder:"images/my_blog_0.png",effect:"fadeIn"})}lazy();
 //rebuild by neat 
