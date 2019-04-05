2dc3ce71692ab01bf6d8d8b34b4b1fc364ce4a18
dataiyangu <32248827@qq.com>
2019-04-05 23:04:44 +0800

    Site updated: 2019-04-05 23:04:42

--- js/src/pjax/comments_git.js
+++ js/src/pjax/comments_git.js
@@ -1,3 +1,3 @@
-// build time:Fri Apr 05 2019 22:46:15 GMT+0800 (China Standard Time)
+// build time:Fri Apr 05 2019 23:04:39 GMT+0800 (China Standard Time)
 if($("#gitalk-container").length>0){var gitalk=new Gitalk({clientID:"a9ce619db1631dda096d",clientSecret:"7e8f7305d2493c153d210082610329f07f1aa093",repo:"dataiyangu.github.io",owner:"dataiyangu",admin:["dataiyangu"],id:location.pathname});gitalk.render("gitalk-container")}
 //rebuild by neat 
