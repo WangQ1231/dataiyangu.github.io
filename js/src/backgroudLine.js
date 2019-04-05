26fc0e4233c1d703f75afb7941588feaeca63176
dataiyangu <32248827@qq.com>
2019-04-06 07:38:30 +0800

    Site updated: 2019-04-06 07:38:28

--- js/src/backgroudLine.js
+++ js/src/backgroudLine.js
@@ -1,3 +1,3 @@
-// build time:Sat Apr 06 2019 07:31:07 GMT+0800 (China Standard Time)
+// build time:Sat Apr 06 2019 07:38:25 GMT+0800 (China Standard Time)
 
 //rebuild by neat 
--- js/src/pjax/scrollReveal/my_scrollReveal.js
+++ js/src/pjax/scrollReveal/my_scrollReveal.js
@@ -1,3 +1,3 @@
-// build time:Sat Apr 06 2019 07:31:07 GMT+0800 (China Standard Time)
+// build time:Sat Apr 06 2019 07:38:25 GMT+0800 (China Standard Time)
 var pathname=window.location.pathname;if(pathname.indexOf("html")<0){var config={reset:true,init:true};window.scrollReveal=new scrollReveal(config);scrollReveal.init()}
 //rebuild by neat 
