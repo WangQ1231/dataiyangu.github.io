8b9bf6ebb6912860c57c795463101d886ca60abe
dataiyangu <32248827@qq.com>
2019-08-04 18:12:50 +0800

    Site updated: 2019-08-04 18:12:47

--- js/src/backgroudLine.js
+++ js/src/backgroudLine.js
@@ -1,3 +1,3 @@
-// build time:Sun Aug 04 2019 18:09:43 GMT+0800 (China Standard Time)
+// build time:Sun Aug 04 2019 18:12:44 GMT+0800 (China Standard Time)
 
 //rebuild by neat 
--- js/src/pjax/lazyload.js
+++ js/src/pjax/lazyload.js
@@ -1,3 +1,3 @@
-// build time:Sun Aug 04 2019 18:09:43 GMT+0800 (China Standard Time)
+// build time:Sun Aug 04 2019 18:12:44 GMT+0800 (China Standard Time)
 function lazy(){$("img").lazyload({threshold:0,placeholder:"images/my_blog_0.png",effect:"fadeIn"})}lazy();
 //rebuild by neat 
