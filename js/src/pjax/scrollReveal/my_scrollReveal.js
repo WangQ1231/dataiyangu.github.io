928c74e3d2ce9705c0125d5fdcf0f1c6574650c5
dataiyangu <32248827@qq.com>
2019-08-04 13:07:59 +0800

    Site updated: 2019-08-04 13:07:56

--- js/src/pjax/scrollReveal/my_scrollReveal.js
+++ js/src/pjax/scrollReveal/my_scrollReveal.js
@@ -1,3 +1,3 @@
-// build time:Sun Aug 04 2019 11:18:52 GMT+0800 (China Standard Time)
+// build time:Sun Aug 04 2019 13:07:53 GMT+0800 (China Standard Time)
 var pathname=window.location.pathname;if(pathname.indexOf("html")<0){var config={reset:true,init:true};window.scrollReveal=new scrollReveal(config);scrollReveal.init()}
 //rebuild by neat 
