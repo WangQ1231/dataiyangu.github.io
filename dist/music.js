71b59a3749dbcfa2cc28c53bb4815c425f146daf
dataiyangu <32248827@qq.com>
2019-08-04 18:09:48 +0800

    Site updated: 2019-08-04 18:09:45

--- dist/music.js
+++ dist/music.js
@@ -1,3 +1,3 @@
-// build time:Sun Aug 04 2019 14:50:53 GMT+0800 (China Standard Time)
+// build time:Sun Aug 04 2019 18:09:43 GMT+0800 (China Standard Time)
 define(function(){const e=new APlayer({container:document.getElementById("aplayer"),fixed:true,theme:"#3C3F41",preload:false,audio:[{name:"第三人称",artist:"买辣椒也用券",url:"http://other.web.nc01.sycdn.kuwo.cn/resource/n2/59/48/1124497297.mp3",cover:"/images/music.png"}]})});
 //rebuild by neat 
