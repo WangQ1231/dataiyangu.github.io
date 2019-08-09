0a80adb7b6604cc1b5fdeb97eac575f69ebbacd6
dataiyangu <32248827@qq.com>
2019-08-09 17:47:20 +0800

    Site updated: 2019-08-09 17:47:12

--- dist/music.js
+++ dist/music.js
@@ -1,3 +1,3 @@
-// build time:Fri Aug 09 2019 17:39:30 GMT+0800 (China Standard Time)
+// build time:Fri Aug 09 2019 17:47:03 GMT+0800 (China Standard Time)
 define(function(){const e=new APlayer({container:document.getElementById("aplayer"),fixed:true,theme:"#3C3F41",preload:false,audio:[{name:"第三人称",artist:"买辣椒也用券",url:"http://other.web.nc01.sycdn.kuwo.cn/resource/n2/59/48/1124497297.mp3",cover:"/images/music.png"}]})});
 //rebuild by neat 
--- js/src/pjax/scrollReveal/my_scrollReveal.js
+++ js/src/pjax/scrollReveal/my_scrollReveal.js
@@ -1,3 +1,3 @@
-// build time:Fri Aug 09 2019 17:39:30 GMT+0800 (China Standard Time)
+// build time:Fri Aug 09 2019 17:47:03 GMT+0800 (China Standard Time)
 var pathname=window.location.pathname;if(pathname.indexOf("html")<0){var config={reset:true,init:true};window.scrollReveal=new scrollReveal(config);scrollReveal.init()}
 //rebuild by neat 
